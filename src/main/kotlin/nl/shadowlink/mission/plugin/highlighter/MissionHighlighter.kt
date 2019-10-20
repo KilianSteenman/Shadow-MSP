2cabbb3b33740bced2d4b514e209fa02fe9d19a2
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2019-10-20 15:44:42 +0200

    Basic highlighter implementation

---
+++ src/main/kotlin/nl/shadowlink/mission/plugin/highlighter/MissionHighlighter.kt
@@ -0,0 +1,26 @@
+package nl.shadowlink.mission.plugin.highlighter
+
+import com.intellij.lexer.Lexer
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
+import com.intellij.psi.tree.IElementType
+import nl.shadowlink.mission.plugin.lexer.MissionLexer
+import nl.shadowlink.mission.plugin.lexer.MissionTokenType
+
+class MissionHighlighter : SyntaxHighlighterBase() {
+
+    override fun getTokenHighlights(tokenType: IElementType?): Array<TextAttributesKey> {
+        return when (tokenType as MissionTokenType) {
+            MissionTokenType.STRING -> arrayOf(DefaultLanguageHighlighterColors.STRING)
+            MissionTokenType.COMMENT -> arrayOf(DefaultLanguageHighlighterColors.LINE_COMMENT)
+            MissionTokenType.MODEL -> arrayOf(DefaultLanguageHighlighterColors.CONSTANT)
+            MissionTokenType.INT -> arrayOf(DefaultLanguageHighlighterColors.NUMBER)
+            MissionTokenType.FLOAT -> arrayOf(DefaultLanguageHighlighterColors.NUMBER)
+            MissionTokenType.LABEL -> arrayOf(DefaultLanguageHighlighterColors.FUNCTION_DECLARATION)
+            else -> emptyArray()
+        }
+    }
+
+    override fun getHighlightingLexer(): Lexer = MissionLexer()
+}
