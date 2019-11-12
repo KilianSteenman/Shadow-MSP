439ff04db11b7da430d62390a2f068af60eab2c5
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2019-11-12 21:38:51 +0100

    Fold mission definitions

--- src/main/kotlin/nl/shadowlink/mission/plugin/folding/MissionFolding.kt
+++ src/main/kotlin/nl/shadowlink/mission/plugin/folding/MissionFolding.kt
@@ -5,15 +5,39 @@ import com.intellij.lang.folding.FoldingBuilderEx
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
 import com.intellij.psi.PsiElement
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.openapi.editor.FoldingGroup
+import nl.shadowlink.mission.plugin.psi.defines.DefineMissionElement
+
 
 class MissionFolding : FoldingBuilderEx() {
 
     override fun getPlaceholderText(node: ASTNode): String? {
-        return "Missions..."
+        return "Missions ..."
     }
 
     override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
-        return emptyArray()
+        val group = FoldingGroup.newGroup("simple")
+
+        val descriptors = ArrayList<FoldingDescriptor>()
+        val defineMissionExpressions = PsiTreeUtil.findChildrenOfType(root, DefineMissionElement::class.java)
+
+        if (defineMissionExpressions.size > 2) {
+            val firstMission = defineMissionExpressions.firstOrNull()
+            val lastMission = defineMissionExpressions.lastOrNull()
+            if (firstMission != null && lastMission != null) {
+                descriptors.add(object : FoldingDescriptor(firstMission.node,
+                        TextRange(firstMission.textRange.startOffset,
+                                lastMission.textRange.endOffset),
+                        group) {
+                    override fun getPlaceholderText(): String? {
+                        return "Missions ..."
+                    }
+                })
+            }
+        }
+        return descriptors.toTypedArray()
     }
 
     override fun isCollapsedByDefault(node: ASTNode): Boolean = true
