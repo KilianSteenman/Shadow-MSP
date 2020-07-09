a67cd3cbdc55cfd1a1c644962e36dc793a5c5773
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2020-07-09 19:05:57 +0200

    Start work on GlobalsCompletionContributor

---
+++ src/main/kotlin/nl/shadowlink/mission/plugin/completion/GlobalsCompletionContributor.kt
@@ -0,0 +1,30 @@
+package nl.shadowlink.mission.plugin.completion
+
+import com.intellij.codeInsight.completion.*
+import com.intellij.codeInsight.lookup.LookupElementBuilder
+import com.intellij.patterns.PlatformPatterns
+import com.intellij.util.ProcessingContext
+import nl.shadowlink.mission.plugin.MissionLanguage
+import nl.shadowlink.mission.plugin.lexer.MissionTokenType
+import nl.shadowlink.mission.plugin.psi.findGlobalDefinitions
+import nl.shadowlink.mission.plugin.psi.findLabelDefinitions
+
+class GlobalsCompletionContributor : CompletionContributor() {
+
+    init {
+        extend(CompletionType.BASIC, PlatformPatterns.psiElement(MissionTokenType.GLOBAL_VAR)
+                .andOr(PlatformPatterns.psiElement(MissionTokenType.GLOBAL_VAR_DEF))
+                .withLanguage(MissionLanguage),
+                GlobalsCompletionProvider()
+        )
+    }
+}
+
+private class GlobalsCompletionProvider : CompletionProvider<CompletionParameters>() {
+
+    override fun addCompletions(parameters: CompletionParameters, context: ProcessingContext, result: CompletionResultSet) {
+        findGlobalDefinitions(parameters.originalFile.project).forEach { global ->
+            result.addElement(LookupElementBuilder.create(global.name))
+        }
+    }
+}
--- src/main/kotlin/nl/shadowlink/mission/plugin/psi/PsiUtils.kt
+++ src/main/kotlin/nl/shadowlink/mission/plugin/psi/PsiUtils.kt
@@ -1,7 +1,6 @@
 package nl.shadowlink.mission.plugin.psi
 
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiFile
 import com.intellij.psi.PsiManager
 import com.intellij.psi.search.FileTypeIndex
@@ -9,18 +8,33 @@ import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.psi.util.PsiTreeUtil
 import nl.shadowlink.mission.plugin.MissionFile
 import nl.shadowlink.mission.plugin.MissionFileType
+import nl.shadowlink.mission.plugin.psi.global.GlobalVarDefinitionElement
+import nl.shadowlink.mission.plugin.psi.global.GlobalVarElement
 import nl.shadowlink.mission.plugin.psi.label.LabelDefinitionElement
 
 fun findLabelDefinitions(project: Project): List<LabelDefinitionElement> {
     return FileTypeIndex.getFiles(MissionFileType, GlobalSearchScope.allScope(project))
-            .mapNotNull { virtualFile -> PsiManager.getInstance(project).findFile(virtualFile) as MissionFile? }
-            .mapNotNull { missionFile -> PsiTreeUtil.getChildrenOfType(missionFile, LabelDefinitionElement::class.java) }
-            .flatMap { definitions -> definitions.asList() }
+            .mapNotNull { virtualFile -> PsiManager.getInstance(project).findFile(virtualFile) as MissionFile }
+            .flatMap { missionFile -> findLabelDefinitions(missionFile) }
 }
 
 fun findLabelDefinitions(file: PsiFile): List<LabelDefinitionElement> {
     return if (file is MissionFile) {
         return PsiTreeUtil.getChildrenOfType(file, LabelDefinitionElement::class.java)?.asList() ?: emptyList()
+    } else {
+        emptyList()
+    }
+}
+
+fun findGlobalDefinitions(project: Project): List<GlobalVarElement> {
+    return FileTypeIndex.getFiles(MissionFileType, GlobalSearchScope.allScope(project))
+            .mapNotNull { virtualFile -> PsiManager.getInstance(project).findFile(virtualFile) as MissionFile }
+            .flatMap { missionFile -> findGlobalDefinitions(missionFile) }
+}
+
+fun findGlobalDefinitions(file: PsiFile): List<GlobalVarElement> {
+    return if (file is MissionFile) {
+        return PsiTreeUtil.getChildrenOfType(file, GlobalVarDefinitionElement::class.java)?.asList() ?: emptyList()
     } else {
         emptyList()
     }
