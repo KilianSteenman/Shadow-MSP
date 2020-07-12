a320857973cacf0b9c23f6a35f726d3002721550
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2020-07-12 10:41:29 +0200

    Implement basic createLabelQuickFix

--- src/main/kotlin/nl/shadowlink/mission/plugin/quickfixes/CreateLabelQuickFix.kt
+++ src/main/kotlin/nl/shadowlink/mission/plugin/quickfixes/CreateLabelQuickFix.kt
@@ -1,10 +1,13 @@
 package nl.shadowlink.mission.plugin.quickfixes
 
 import com.intellij.codeInsight.intention.impl.BaseIntentionAction
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiFile
 import nl.shadowlink.mission.plugin.psi.label.LabelReferenceElement
+import org.jdom.filter2.Filters.document
+
 
 class CreateLabelQuickFix(
         private val labelReferenceElement: LabelReferenceElement
@@ -14,9 +17,12 @@ class CreateLabelQuickFix(
 
     override fun getFamilyName(): String = "Label name"
 
-    override fun isAvailable(project: Project, editor: Editor?, file: PsiFile?): Boolean = true
+    override fun isAvailable(project: Project, editor: Editor, file: PsiFile): Boolean = true
 
-    override fun invoke(project: Project, editor: Editor?, file: PsiFile?) {
-        // TODO: Implement element creation
+    override fun invoke(project: Project, editor: Editor, file: PsiFile) {
+        WriteCommandAction.runWriteCommandAction(project) {
+            editor.document.insertString(editor.document.textLength, "\n\n:${labelReferenceElement.name}\n")
+            editor.caretModel.moveToOffset(editor.document.textLength)
+        }
     }
 }
