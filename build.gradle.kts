a8a01e8f568b3ac23188aae7d4aa6b4bf4a8ae2d
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2023-01-31 19:29:38 +0100

    Update Kotlin to 1.8.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.compose.compose
 
 buildscript {
     repositories { mavenCentral() }
-    dependencies { classpath(kotlin("gradle-plugin", "1.7.20")) }
+    dependencies { classpath(kotlin("gradle-plugin", "1.8.0")) }
 }
 
 plugins {
-    id("org.jetbrains.intellij") version "1.10.2"
-    id("org.jetbrains.kotlin.jvm") version "1.7.20"
-    id("org.jetbrains.compose") version "1.2.1"
+    id("org.jetbrains.intellij") version "1.12.0"
+    id("org.jetbrains.kotlin.jvm") version "1.8.0"
+    id("org.jetbrains.compose") version "1.3.0"
 }
 
 repositories {
