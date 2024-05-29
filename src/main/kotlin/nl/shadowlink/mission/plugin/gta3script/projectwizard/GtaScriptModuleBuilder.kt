7991613d325b439a4bb7c13747441dfbb0c2fed0
Kilian Steenman <37879201+KilianSteenman@users.noreply.github.com>
2024-05-29 21:42:12 +0200

    Game Type select screen

--- src/main/kotlin/nl/shadowlink/mission/plugin/gta3script/projectwizard/GtaScriptModuleBuilder.kt
+++ src/main/kotlin/nl/shadowlink/mission/plugin/gta3script/projectwizard/GtaScriptModuleBuilder.kt
@@ -2,24 +2,37 @@ package nl.shadowlink.mission.plugin.gta3script.projectwizard
 
 import com.intellij.ide.util.projectWizard.ModuleBuilder
 import com.intellij.ide.util.projectWizard.ModuleWizardStep
+import com.intellij.ide.util.projectWizard.SettingsStep
 import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.module.ModuleType
+import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.openapi.roots.ui.configuration.ModulesProvider
+import com.intellij.ui.dsl.builder.panel
+import nl.shadowlink.mission.plugin.GameType
+import javax.swing.DefaultComboBoxModel
+import javax.swing.JComboBox
+import javax.swing.JComponent
 
 class GtaScriptModuleBuilder : ModuleBuilder() {
 
     override fun getModuleType(): ModuleType<*> = GtaScriptModuleType()
 
-    override fun getCustomOptionsStep(context: WizardContext?, parentDisposable: Disposable?): ModuleWizardStep? {
-        return GtaScriptTypeWizardStep()
-    }
+    override fun getCustomOptionsStep(context: WizardContext?, parentDisposable: Disposable?): ModuleWizardStep {
+        return object : ModuleWizardStep() {
+            val gameTypeComboBoxModel = DefaultComboBoxModel(GameType.entries.toTypedArray())
 
-//    override fun setupRootModel(modifiableRootModel: ModifiableRootModel) {
-//        doAddContentEntry(modifiableRootModel)
-//
-//        val contentEntryPath = contentEntryPath ?: return
-//        LocalFileSystem.getInstance().findFileByPath(contentEntryPath) ?: return
-//
-//        type?.moduleCreator?.createModule(modifiableRootModel)
-//    }
+            override fun getComponent(): JComponent {
+                return panel {
+                    row("GameType") {
+                        comboBox(gameTypeComboBoxModel)
+                    }
+                }
+            }
+
+            override fun updateDataModel() {
+                TODO("Not yet implemented")
+            }
+        }
+    }
 }
